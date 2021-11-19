6401a8eb59b1de7e751713d60766e5153048c0b6
amiller1890 <83983395+amiller1890@users.noreply.github.com>
2021-11-19 17:05:10 -0500

    Add project files.

---
+++ THEplantAPP/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace THEplantAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.plantNameTextBox = new System.Windows.Forms.TextBox();
+            this.sunTextBox = new System.Windows.Forms.TextBox();
+            this.plantTypeTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hardBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plant Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sun Needed:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Plant Type:";
+            // 
+            // plantNameTextBox
+            // 
+            this.plantNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plantNameTextBox.Location = new System.Drawing.Point(219, 72);
+            this.plantNameTextBox.Name = "plantNameTextBox";
+            this.plantNameTextBox.Size = new System.Drawing.Size(169, 28);
+            this.plantNameTextBox.TabIndex = 3;
+            // 
+            // sunTextBox
+            // 
+            this.sunTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sunTextBox.Location = new System.Drawing.Point(219, 122);
+            this.sunTextBox.Name = "sunTextBox";
+            this.sunTextBox.ReadOnly = true;
+            this.sunTextBox.Size = new System.Drawing.Size(132, 28);
+            this.sunTextBox.TabIndex = 4;
+            // 
+            // plantTypeTextBox
+            // 
+            this.plantTypeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plantTypeTextBox.Location = new System.Drawing.Point(219, 168);
+            this.plantTypeTextBox.Name = "plantTypeTextBox";
+            this.plantTypeTextBox.ReadOnly = true;
+            this.plantTypeTextBox.Size = new System.Drawing.Size(132, 28);
+            this.plantTypeTextBox.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(132, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Search!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxResult.Location = new System.Drawing.Point(40, 357);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(374, 28);
+            this.textBoxResult.TabIndex = 7;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(494, 34);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(550, 550);
+            this.webBrowser1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(139, 329);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Plant Hardiness";
+            // 
+            // hardBtn
+            // 
+            this.hardBtn.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.hardBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardBtn.Location = new System.Drawing.Point(40, 441);
+            this.hardBtn.Name = "hardBtn";
+            this.hardBtn.Size = new System.Drawing.Size(374, 36);
+            this.hardBtn.TabIndex = 10;
+            this.hardBtn.Text = "Click if you want hardiness values";
+            this.hardBtn.UseVisualStyleBackColor = false;
+            this.hardBtn.Click += new System.EventHandler(this.hardBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(1223, 683);
+            this.Controls.Add(this.hardBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.plantTypeTextBox);
+            this.Controls.Add(this.sunTextBox);
+            this.Controls.Add(this.plantNameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox plantNameTextBox;
+        private System.Windows.Forms.TextBox sunTextBox;
+        private System.Windows.Forms.TextBox plantTypeTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button hardBtn;
+    }
+}
+
---
+++ THEplantAPP/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net;
+
+namespace THEplantAPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect = new SqlConnection("Data Source=ALFY\\SQLEXPRESS;Initial Catalog=PlantDB;Integrated Security=True");
+            connect.Open();
+
+            SqlCommand command1 = new SqlCommand("SELECT Plant_SunlightNeeded, Plant_Type FROM [dbo].[PlantKey] WHERE Plant_Name =@plantNameTextBox", connect);
+            command1.Parameters.AddWithValue("@plantNameTextBox", string.Copy(plantNameTextBox.Text));
+            SqlDataReader reader = command1.ExecuteReader();
+            while (reader.Read())
+            {
+                sunTextBox.Text = reader.GetValue(0).ToString();
+                plantTypeTextBox.Text = reader.GetValue(1).ToString();
+            }
+
+
+            if (plantNameTextBox.Text == "Lavender")
+            {
+                webBrowser1.Navigate("https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/lavandula_sweet_romance_apj18_10.jpg");
+            }
+            if (plantNameTextBox.Text == "Golden Rod")
+            {
+                webBrowser1.Navigate("https://www2.ic.edu/prairie/goldenrod%209.jpg");
+            }
+            if (plantNameTextBox.Text == "Rose")
+            {
+                webBrowser1.Navigate("https://www.gardeningknowhow.com/wp-content/uploads/2019/11/red-rose-400x265.jpg");
+            }
+            if (plantNameTextBox.Text == "Basil")
+            {
+                webBrowser1.Navigate("https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/6804_ocimumgreenbasil_100207.jpg");
+            }
+            if (plantNameTextBox.Text == "Salvia")
+            {
+                webBrowser1.Navigate("https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/6804_ocimumgreenbasil_100207.jpg");
+            }
+            if (plantNameTextBox.Text == "Asters")
+            {
+                webBrowser1.Navigate("https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/aster-flowers.jpg?itok=3bl-Stkz");
+            }
+            if (plantNameTextBox.Text == "Cilantro")
+            {
+                webBrowser1.Navigate("https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/66954.jpg");
+            }
+            if (plantNameTextBox.Text == "Sage")
+            {
+                webBrowser1.Navigate("https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/55636.jpg");
+            }
+            if (plantNameTextBox.Text == "Sunflowers")
+            {
+                webBrowser1.Navigate("https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/helianthus_suncredible_suncredible_yellow.jpg");
+            }
+            if (plantNameTextBox.Text == "Tulips")
+            {
+                webBrowser1.Navigate("https://s3.eu-west-2.amazonaws.com/growinginteractive/plants/TUL.jpg");
+            }
+            if (plantNameTextBox.Text == "Oak Tree")
+            {
+                webBrowser1.Navigate("https://cdn.shopify.com/s/files/1/0059/8835/2052/products/Shumard_Oak_600x600_f28082d9-6726-4ad7-9de2-eb3aefc65ceb_grande.jpg?v=1612444334");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hardBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect = new SqlConnection("Data Source=ALFY\\SQLEXPRESS;Initial Catalog=PlantDB;Integrated Security=True");
+            connect.Open();
+
+            SqlCommand command1 = new SqlCommand("SELECT Plant_Hardiness FROM [dbo].[PlantKey] WHERE Plant_Name =@plantNameTextBox", connect);
+            command1.Parameters.AddWithValue("@plantNameTextBox", string.Copy(plantNameTextBox.Text));
+            SqlDataReader reader = command1.ExecuteReader();
+            while (reader.Read())
+            {
+                textBoxResult.Text = reader.GetValue(0).ToString();
+            }
+
+
+        }
+    }
+}
